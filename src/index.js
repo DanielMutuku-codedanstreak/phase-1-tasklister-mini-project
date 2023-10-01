231d7dc5c3e5b5a862618390e5b9ba6d0d31947f
Daniel Mutuku <danielmakau507@gmail.com>
2023-10-01 21:15:04 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form')
+  const inputText = document.getElementById('new-task-description')
+  const taskList = document.getElementById('tasks')
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log(inputText.value)//display input in DOM
+    //add items inputted in todo list
+    const taskText = inputText.value;
+    addTask(taskText);
+    inputText.value = '' //to clear input field after input
+  });
+
+  function addTask(text) {
+    const taskItem = document.createElement('li');
+    taskItem.textContent = text;
+    // Add a delete button
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', function () {
+      taskItem.remove(); // Remove the task when the delete button is clicked
+    });
+
+    taskItem.appendChild(deleteButton);
+    taskList.appendChild(taskItem);
+  }
+
 });
